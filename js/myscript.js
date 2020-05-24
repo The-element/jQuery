d7c4395c1bb351d2e2758415a01e7782a58d31f8
The-element <nicke.jonsson@live.com>
2020-05-24 16:47:14 +0000

    finished

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
-    
+    $("h2").removeClass("underline"); //removes the underline class from all the h2 elements
+    $("ul").removeClass("border"); //removes all the border class from all Lists.
 });
+
+
+/*
+$(document).ready(function() {
+    $("h2").addClass("underline"); //underlines all the h2 elements
+    $("ul").addClass("border"); //Adds a border to all Lists.
+});
+
+
+$(document).ready(function() {
+    $("h2").css("text-decoration","underline"); //underlines all the h2 elements
+    $("ul").css("border", "solid 1px #ccc"); //Adds a border to all Lists.
+});
+*/
